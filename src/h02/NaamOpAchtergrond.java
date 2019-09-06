9fad71f1bb737edc3a073fe345a784d6f6ef6362
LyxurD4 <mdoes@roc-dev.com>
2019-09-06 10:54:47 +0200

    Vragen H2 bijgevoegd

--- src/h02/NaamOpAchtergrond.java
+++ src/h02/NaamOpAchtergrond.java
@@ -1,5 +1,20 @@
 package h02;
 
+
+//Wat is machinetaal?
+//Machinetaal is een taal waarin opdrahten geveven worden voor de processor van de computer
+
+//Welke drie hulpmiddelen heb je nodig om te progammeren?
+//een editor, geïmporteerde klassen en code
+
+//Hoe herken je opdrachten in de broncode?
+//Door tussen de accolades te kijken in de code
+
+//Waartoe dienen de import-opdrachten?
+//Import opdrachten kunnen code die al is geschreven gebruiken om zo de code makkeijker en korter te houden
+
+//Wat is een methode?
+//Een methode zijn een aantal opdrachten met een naam erboven
 import java.awt.*;
 import java.applet.*;
 
