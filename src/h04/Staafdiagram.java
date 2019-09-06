3a6446dd1cc86de66c0d3fea583bdb72eff55078
LyxurD4 <mdoes@roc-dev.com>
2019-09-06 13:28:16 +0200

    Staafdiagram geüpdated

--- src/h04/Staafdiagram.java
+++ src/h04/Staafdiagram.java
@@ -10,8 +10,12 @@ public class Staafdiagram extends Applet {
 
     public void paint(Graphics g) {
         setBackground(Color.white);
+
+        //Lijnen van de grafiek
         g.drawLine(60, 20, 60, 480);
         g.drawLine(60, 480, 400, 480);
+
+        //Schaalverdeling
         g.drawString("0", 50, 480);
         g.drawString("20", 45, 400);
         g.drawString("40", 45, 320);
@@ -19,12 +23,20 @@ public class Staafdiagram extends Applet {
         g.drawString("80", 45, 160);
         g.drawString("100", 35, 80);
         g.drawString("120", 35, 20);
+
+        //Valerie
         g.setColor(Color.magenta);
         g.fillRect(120, 320, 30, 160);
         g.drawString("Valerie", 110, 500);
+
+        //Jeroen
         g.setColor(Color.cyan);
         g.fillRect(180, 80, 30, 400);
         g.drawString("Jeroen", 170, 500);
+
+        //Hans
         g.setColor(Color.red);
         g.fillRect(240, 160, 30, 320);
         g.drawString("Hans", 240, 500);
+    }
+}
